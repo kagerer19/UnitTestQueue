a9a7eeaa7152964b07ddeacd7a3deba63803d44b
Alexander kagerer <kagerer19@gmail.com>
2023-09-07 09:16:40 +0200

    Add test files

---
+++ src/test/java/TheQueue/IntQueueTest.java
@@ -0,0 +1,28 @@
+package TheQueue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntQueueTest {
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+
+    @Test
+    void testDequeue() {
+    }
+}
